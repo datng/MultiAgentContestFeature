b282d3ed9f5dfe90894c5362c3699a3e6c2553f1
Loubout <louis.k.boutin@gmail.com>
2016-05-24 16:15:36 +0200

    minimal myAgent

--- features/MultiAgentContestFeature/MyAgent.java
+++ features/MultiAgentContestFeature/MyAgent.java
@@ -3,25 +3,54 @@ import massim.javaagents.agents.CityUtil;
 import eis.iilang.Action;
 import eis.iilang.Percept;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class MyAgent extends Agent {
+
+	private boolean startRole = false;
+	private Role r;
 
 	public MyAgent(String name, String team) {
 		super(name, team);
-		// TODO Auto-generated constructor stub
+
+
+
 	}
 
 	@Override
-	public Action step() {
-		// TODO deliberate and Parameter action	
-		for(Percept p : getAllPercepts())
-			handlePercept(p);
-		
+	public Action step() {	
+		if (!startRole){
+			for(Percept p : getAllPercepts())
+				if(p.getName() == "entity"){
+					String selfType = p.getParameters().getLast().toString();
+					// selfType contains "truck", "drone", "motorbike", "car"
+					if (selfType == "truck"){
+						this.r = Role.Acheteur;
+						break;
+					}else if (selfType == "drone"){
+						this.r = Role.JobDeliver;
+						break;
+					}else if (selfType == "motorbike"){
+						this.r = Role.Acheteur;
+						break;
+					}else if (selfType == "car"){
+						this.r = Role.Assembleur;
+						break;
+					}else{
+						System.out.println("FAIL SUR START ROLE");
+					}
+				
+				}
+		}
+
 		return CityUtil.action("goto", "facility=shop1");
 	}
 
+
 	@Override
 	public void handlePercept(Percept p) {
-		
+
 	}
 
 }
